ae0cadfe0da215d8a8e9f174254b1622393652a7
Kiran George <gaurdian.kiran@gmail.com>
2024-12-03 16:11:15 +0100

    game of life done

--- Assets/Script/GridCreation.cs
+++ Assets/Script/GridCreation.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = System.Random;
 
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshFilter))]
@@ -12,15 +14,34 @@ public class GridCreation : MonoBehaviour
 
     private MeshRenderer _meshRenderer;
     private MeshFilter _meshFilter;
-
-    private int[,] _gridData;
+    
+    private Tile[,] _tileData;
+    private List<Color> _colorData;
 
     private void Awake()
     {
-        _gridData = new int[Width, Height];
+        _tileData = new Tile[Width, Height];
         _meshRenderer = GetComponent<MeshRenderer>();
         _meshFilter = GetComponent<MeshFilter>();
+        GenerateTileData();
         GenerateMeshData();
+        UpdateMeshColorData();
+        StartCoroutine(DoGameOfLife());
+    }
+
+    private void GenerateTileData()
+    {
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                Tile currentTile = new Tile();
+                currentTile.X = (UInt16)i;
+                currentTile.Y = (UInt16)j;
+                currentTile.isAlive = UnityEngine.Random.Range(0, 2) > 0;
+                _tileData[i, j] = currentTile;
+            }
+        }
     }
 
     private void GenerateMeshData()
@@ -28,18 +49,20 @@ public class GridCreation : MonoBehaviour
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
         List<Vector3> normals = new List<Vector3>();
+        _colorData = new List<Color>();
 
         for (int i = 0; i < Height; i++)
         {
             for (int j = 0; j < Width; j++)
             {
                 int numberOfVertices = vertices.Count;
+                Tile currentTile = _tileData[i, j];
                 vertices.AddRange(new Vector3[]
                 {
-                    new Vector3(i, j, 0),
-                    new Vector3(i+1, j, 0),
-                    new Vector3(i, j+1, 0),
-                    new Vector3(i+1, j+1, 0)
+                    new Vector3(currentTile.X, currentTile.Y, 0),
+                    new Vector3(currentTile.X + 1, currentTile.Y, 0),
+                    new Vector3(currentTile.X, currentTile.Y + 1, 0),
+                    new Vector3(currentTile.X + 1, currentTile.Y + 1, 0)
                 });
                 triangles.AddRange(new int[]
                 {
@@ -54,8 +77,72 @@ public class GridCreation : MonoBehaviour
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.normals = normals.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
         _meshFilter.mesh = mesh;
     }
 
+    private void UpdateMeshColorData()
+    {
+        _colorData.Clear();
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                Color colorToBeAdded = _tileData[i, j].isAlive ? Color.black : Color.white;
+                _colorData.AddRange(new Color[]
+                {
+                    colorToBeAdded, colorToBeAdded, colorToBeAdded, colorToBeAdded,
+                });
+            }
+        }
+        _meshFilter.mesh.colors = _colorData.ToArray();   
+    }
 
+    private IEnumerator DoGameOfLife()
+    {
+        while (true)
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Tile currentTile = _tileData[i, j];
+                    int neighboursOfTile = CheckNeighbours(currentTile);
+                    if (currentTile.isAlive)
+                    {
+                        if(neighboursOfTile > 3)
+                            currentTile.isAlive = false;
+                        else if(neighboursOfTile < 2)
+                            currentTile.isAlive = false;
+                        else if(neighboursOfTile == 2 || neighboursOfTile == 3)
+                            currentTile.isAlive = true;
+                    }
+                    else
+                    {
+                        if (neighboursOfTile == 3)
+                            currentTile.isAlive = true;
+                    }
+                }
+            }
+            UpdateMeshColorData();
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+    }
+
+    private int CheckNeighbours(Tile tile)
+    {
+        int aliveNeighbors = 0;
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if((i == 0 && j == 0) || tile.X + i >= Height - 1 || tile.Y + j >= Width - 1 || tile.X + i <= 0 || tile.Y + j <= 0)
+                    continue;
+                if(_tileData[tile.X + i, tile.Y + j].isAlive)
+                    aliveNeighbors++;
+            }
+        }
+        return aliveNeighbors;
+    }
 }
---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Tile
+{
+    public bool isAlive;
+    public UInt16 X;
+    public UInt16 Y;
+}
