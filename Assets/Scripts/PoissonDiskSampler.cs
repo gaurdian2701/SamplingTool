533d91f66d2eb17c0dc50f4c6e2efba59039ad6c
Kiran George <gaurdian.kiran@gmail.com>
2025-02-23 12:47:23 +0100

    Have to work on using KD-Trees

--- Assets/Scripts/PoissonDiskSampler.cs
+++ Assets/Scripts/PoissonDiskSampler.cs
@@ -14,26 +14,63 @@ public class PoissonDiskSampler : MonoBehaviour
     
     private const string GROUND_LAYER = "Ground";
     private const float Y_OFFSET_FOR_SAMPLING_RAYCAST = 2f;
+    
+    private List<Vector3?> _activePoints = new List<Vector3?>();
 
     public void DoSampling()
     {
         //Get a random point on the mesh
-        Vector3 randomPointOnMesh = GetRandomPointOnMesh();
-        if (randomPointOnMesh == Vector3.negativeInfinity)
+        Vector3? randomPointOnMesh = GetRandomPointOnMesh();
+        if (randomPointOnMesh == null)
+        {
+            Debug.LogWarning("Could not find random point on mesh. Please move the collider to an appropriate position.");
             return;
+        }
+        _activePoints.Add(randomPointOnMesh);
+
+        while(_activePoints.Count > 0)
+        {
+            Vector3? sampledPoint = _activePoints[Random.Range(0, _activePoints.Count)];
+        }
     }
 
-    private Vector3 GetRandomPointOnMesh()
+    private Vector3? GetRandomPointOnMesh()
     {
         Vector3 colliderExtentsOffset = new Vector3(Random.Range(0, PoissonCollider.bounds.extents.x), 
             PoissonCollider.bounds.extents.y,
             Random.Range(0, PoissonCollider.bounds.extents.z));
-        Vector3 rayCastOriginPoint = transform.position + colliderExtentsOffset;
-        rayCastOriginPoint.y *= Y_OFFSET_FOR_SAMPLING_RAYCAST;
-        Physics.Raycast(rayCastOriginPoint, Vector3.down, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask(GROUND_LAYER));
-        if (hit.collider != null)
-            return hit.point;
-        else
-            return Vector3.negativeInfinity;
+        RaycastHit hitInfo = new RaycastHit();
+        if (TryRayCast(transform.position, colliderExtentsOffset, ref hitInfo))
+            return hitInfo.point;
+        return null;
+    }
+
+    private bool PointIsValid(Vector3 point)
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        for (sbyte i = 0; i < SampleLimit; i++)
+        {
+            Vector3 sampledPoint = GetRandomPointOnDisk(point);
+            if (!TryRayCast(sampledPoint, Vector3.zero, ref hitInfo)) continue;
+            return false;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetRandomPointOnDisk(Vector3 point)
+    {
+        //Getting a random point on the disk surrounding the sampled point
+        float randomAngleAroundSampledPoint = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+        Vector3 randomDirectionalVector = new Vector3(Mathf.Cos(randomAngleAroundSampledPoint), 0f, Mathf.Sin(randomAngleAroundSampledPoint)).normalized *
+                                          Random.Range(SamplingRadius, SamplingRadius * 2f);
+        return point + randomDirectionalVector;
+    }
+
+    private bool TryRayCast(Vector3 rayOrigin, Vector3 offset, ref RaycastHit hitInfo)
+    {
+        Vector3 originWithOffset = rayOrigin + offset;
+        originWithOffset.y *= Y_OFFSET_FOR_SAMPLING_RAYCAST;
+        return Physics.Raycast(originWithOffset, Vector3.down, out hitInfo, Mathf.Infinity, LayerMask.GetMask(GROUND_LAYER));
     }
 }
